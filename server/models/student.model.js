4eec5c85cfd1ec3d95a360dee4349c8f1e2db60d
Rafal Pijet <stronglopez@wp.pl>
2019-11-04 14:23:43 +0100

    Add student model

---
+++ server/models/student.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Student = new Schema({
+    id: {type: "String", required: true},
+    firstName: {type: "String", required: true},
+    lastName: {type: "String", required: true},
+    birthDate: {type: "Date", required: true},
+    ratings: [{type: Schema.ObjectId, ref: "Rating"}],
+    parents: [{type: Schema.ObjectId, ref: "User"}]
+});
+
+function populateRatings(next) {
+    this.populate('ratings');
+    next();
+}
+
+function populateUsers(next) {
+    this.populate('parents');
+    next();
+}
+
+Student.pre('find', populateRatings);
+Student.pre('find', populateUsers);
+
+module.exports = mongoose.model("Student", Student);
