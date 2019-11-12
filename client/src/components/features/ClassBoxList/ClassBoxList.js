fd948ad2156f4534fdf2ee3d10f89b847486eca1
Rafal Pijet <stronglopez@wp.pl>
2019-11-12 10:28:49 +0100

    Add request handling in ClassBoxList component

--- client/src/components/features/ClassBoxList/ClassBoxList.js
+++ client/src/components/features/ClassBoxList/ClassBoxList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ClassBox from '../../features/ClassBox/ClassBox';
+import Spinner from '../../common/Spinner/Spinner';
+import Alert from '../../common/Alert/Alert';
 
 class ClassBoxList extends React.Component {
     constructor(props) {
@@ -8,21 +10,30 @@ class ClassBoxList extends React.Component {
         this.props.loadClasses(this.props.user.id);
         console.log('wow');
     }
+
     render() {
-        const {classes} = this.props;
-        return (
-            <ul>
-                {classes.map((diary, i) => {
-                    return <ClassBox key={i} diary={diary}/>
-                })}
-            </ul>
-        )
+        const {classes, request} = this.props;
+
+        if (request.pending) {
+            return <Spinner/>
+        } else if (!request.pending && request.error) {
+            return <Alert variant="error" isVisible={true}>{request.error}</Alert>
+        } else {
+            return (
+                <ul>
+                    {classes.map((diary, i) => {
+                        return <ClassBox key={i} diary={diary}/>
+                    })}
+                </ul>
+            )
+        }
     }
 }
 
 ClassBoxList.propTypes = {
     classes: PropTypes.array.isRequired,
     user: PropTypes.object.isRequired,
+    request: PropTypes.object.isRequired,
     loadClasses: PropTypes.func.isRequired
 };
 
--- client/src/components/pages/ClassDiaries/ClassDiaries.js
+++ client/src/components/pages/ClassDiaries/ClassDiaries.js
@@ -2,23 +2,26 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {getTeacherAllClass} from "../../../redux/actions/classActions";
 import {getUser} from "../../../redux/actions/usersActions";
+import {getRequest} from "../../../redux/actions/requestActions";
 import {loadAllClassByTeacherId} from "../../../redux/thunks";
 import PageTitle from '../../common/PageTitle/PageTitle';
 import ClassBoxList from '../../features/ClassBoxList/ClassBoxList';
 
 const ClassDiaries = props => {
-    const {loadClasses, user, classes} = props;
+    const {loadClasses, user, classes, request} = props;
     return (
         <div>
             <PageTitle>Class Diaries</PageTitle>
-            <ClassBoxList classes={classes} user={user} loadClasses={loadClasses}/>
+            <ClassBoxList classes={classes} user={user}
+                          loadClasses={loadClasses} request={request}/>
         </div>
     )
 };
 
 const mapStateToProps = state => ({
     classes: getTeacherAllClass(state),
-    user: getUser(state)
+    user: getUser(state),
+    request: getRequest(state)
 });
 
 const mapDispatchToProps = dispatch => ({
