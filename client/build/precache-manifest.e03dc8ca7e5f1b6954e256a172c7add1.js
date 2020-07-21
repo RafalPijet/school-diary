849a359c31b615cd090be1b55866c135c42af458
Rafal Pijet <stronglopez@wp.pl>
2020-07-21 16:37:28 +0200

    Try fix heroku deployment problem

---
+++ client/build/precache-manifest.e03dc8ca7e5f1b6954e256a172c7add1.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e307d2bdc98b631a656a426bb67b92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8ad203dd99794fb290d",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "34755fa4bed3c006876f",
+    "url": "/static/css/main.609bdf73.chunk.css"
+  },
+  {
+    "revision": "a8ad203dd99794fb290d",
+    "url": "/static/js/2.50d32c84.chunk.js"
+  },
+  {
+    "revision": "34755fa4bed3c006876f",
+    "url": "/static/js/main.cc1a828c.chunk.js"
+  },
+  {
+    "revision": "6a6e74a0c7f5af1e4e76",
+    "url": "/static/js/runtime-main.e0d478c5.js"
+  },
+  {
+    "revision": "6de5eea4d439f1bef797f5ee21705c16",
+    "url": "/static/media/backParentDark.6de5eea4.jpg"
+  },
+  {
+    "revision": "b23944d8cd8468731c136fdded48a198",
+    "url": "/static/media/backParentLight.b23944d8.jpg"
+  },
+  {
+    "revision": "876ac37b16b6ef9cac0e38ef9fa9d251",
+    "url": "/static/media/backPrincipalDark.876ac37b.jpg"
+  },
+  {
+    "revision": "8795e1fc3e13ec86088d79e369ee2cfd",
+    "url": "/static/media/backPrincipalLight.8795e1fc.jpg"
+  },
+  {
+    "revision": "75f967c43a2063e9100f2338a29c4daa",
+    "url": "/static/media/backTeacherDark.75f967c4.jpg"
+  },
+  {
+    "revision": "55ff996076652a0d2b961ddf89ba6f9c",
+    "url": "/static/media/backTeacherLight.55ff9960.jpg"
+  },
+  {
+    "revision": "c490548d7ed844dc869bb733d915737a",
+    "url": "/static/media/classDiares.c490548d.jpg"
+  },
+  {
+    "revision": "572e54a75f704cde87176c05bc5cdb67",
+    "url": "/static/media/classes.572e54a7.png"
+  },
+  {
+    "revision": "f24d3a00515e38588e9a7f9cb3b8c809",
+    "url": "/static/media/diaryLogo.f24d3a00.png"
+  },
+  {
+    "revision": "259cd116b1cbac03344cadd497524c2b",
+    "url": "/static/media/parent.259cd116.png"
+  },
+  {
+    "revision": "d0e11e5025ce4a4f49d035905431f427",
+    "url": "/static/media/parents.d0e11e50.png"
+  },
+  {
+    "revision": "b75e9dee96919ece24c2810f9fe0d200",
+    "url": "/static/media/principalTeachers.b75e9dee.png"
+  },
+  {
+    "revision": "19d18f804166955fecc71fdd020d056c",
+    "url": "/static/media/ratings.19d18f80.jpg"
+  },
+  {
+    "revision": "8446ab8953f3b759835181516602e2e8",
+    "url": "/static/media/showImage1.8446ab89.png"
+  },
+  {
+    "revision": "035d4978d581bbe6dc22b5b7ab55baea",
+    "url": "/static/media/showImage10.035d4978.png"
+  },
+  {
+    "revision": "16239be9783ede4a146dd81a8a413b35",
+    "url": "/static/media/showImage11.16239be9.png"
+  },
+  {
+    "revision": "96c4bd1bda6917d5828d4c5d534f9543",
+    "url": "/static/media/showImage12.96c4bd1b.png"
+  },
+  {
+    "revision": "34ba378fd3c5ed47442f51805f997c0d",
+    "url": "/static/media/showImage13.34ba378f.png"
+  },
+  {
+    "revision": "2a54ea569313ca73fde5e5f5771aecca",
+    "url": "/static/media/showImage14.2a54ea56.png"
+  },
+  {
+    "revision": "03216128fea63b0815a18d30213366f2",
+    "url": "/static/media/showImage2.03216128.png"
+  },
+  {
+    "revision": "23afe85b8f9962f355632d4aa40e9e13",
+    "url": "/static/media/showImage3.23afe85b.png"
+  },
+  {
+    "revision": "7f5f6703df88d9155ca471cc26a6889e",
+    "url": "/static/media/showImage4.7f5f6703.png"
+  },
+  {
+    "revision": "6cfdcfe661f744844b953b0e6d5e3904",
+    "url": "/static/media/showImage5.6cfdcfe6.png"
+  },
+  {
+    "revision": "6a19ba9db1370c281bde03d8066e5c0c",
+    "url": "/static/media/showImage6.6a19ba9d.png"
+  },
+  {
+    "revision": "219dec97dff17b8a086c09b15feb5f70",
+    "url": "/static/media/showImage7.219dec97.png"
+  },
+  {
+    "revision": "8c90fda31933b4678d3e909b5b21c156",
+    "url": "/static/media/showImage8.8c90fda3.png"
+  },
+  {
+    "revision": "94f19c5e1f48a6995d4200e3b5b84e53",
+    "url": "/static/media/showImage9.94f19c5e.png"
+  },
+  {
+    "revision": "6e24284c4116a5cfa54b9e19fd53320c",
+    "url": "/static/media/students.6e24284c.png"
+  },
+  {
+    "revision": "1780bd216bbdf161e7b35d5cd40e0fc4",
+    "url": "/static/media/teacherData.1780bd21.jpg"
+  },
+  {
+    "revision": "eea30744a27e0f16f6cdf47b94705d90",
+    "url": "/static/media/teacherDesk.eea30744.png"
+  },
+  {
+    "revision": "900b077e6917157cce8210a0b6785bed",
+    "url": "/static/media/teachers.900b077e.png"
+  }
+]);
