a5ed39b728a31345b3e91d991423b3342bc03b5b
Rafal Pijet <stronglopez@wp.pl>
2020-07-21 10:04:23 +0200

    Add helmet and sanitize to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
+const sanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 const loadTestData = require('./dataTest');
 const userRouter = require('./routes/user.routes');
 const classRouter = require('./routes/class.routes');
@@ -12,12 +14,14 @@ const config = require('./config');
 const app = express();
 
 app.use(cors());
+app.use(sanitize());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use('/api', userRouter);
 app.use('/api', classRouter);
 app.use('/api', ratingRouter);
 app.use('/api', studentRouter);
+app.use(helmet());
 app.use(express.static(path.join(__dirname, '/../client/build')));
 
 mongoose.connect(config.DB, {
@@ -29,6 +33,8 @@ db.once('open', () => {
     console.log('Connected to database');
     loadTestData();
 });
+
+db.on('error', err => console.log("Error connection: " + err));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../client/build/index.html'))
