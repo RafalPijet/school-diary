6461d286b352d236aaa9a0809f2b5fd076c90492
Rafal Pijet <stronglopez@wp.pl>
2020-07-21 12:29:42 +0200

    Try fix heroku deployment problem

--- server/config.js
+++ server/config.js
@@ -1,5 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 7000,
-    // DB: process.env.DB || 'mongodb://localhost:27017/school-diary'
-    DB: 'mongodb+srv://LopezPL:TechnoParty2204@cluster0-cpzdn.gcp.mongodb.net/school?retryWrites=true&w=majority'
+    DB: process.env.DB || 'mongodb://localhost:27017/school-diary'
 };
