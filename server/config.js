c9cb55bf41f1ee9e2edebb3d40380be3ba4396f9
Rafal Pijet <stronglopez@wp.pl>
2020-07-21 08:57:16 +0200

    Change access data to config.sys

--- server/config.js
+++ server/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    // PORT: process.env.PORT || 8000,
-    // DB: process.env.DB || 'mongodb://localhost:27017/school-diary',
-    PORT: 8000,
-    DB: 'mongodb+srv://LopezPL:TechnoParty2204@cluster0-cpzdn.gcp.mongodb.net/school?retryWrites=true&w=majority'
+    PORT: process.env.PORT || 8000,
+    DB: process.env.DB
 };
